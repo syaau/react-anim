cc520147f4a0a9b4beef9176571ce75769641044
Ranjan Shrestha <ranjan@sharingapples.com>
2016-02-17 00:46:33 +0545

    Added file loader for loading images in the demo

--- src/TimedAnimation.js
+++
@@ -1,9 +0,0 @@
-import Animation from './Animation';
-
-class TimedAnimation extends Animation {
-  calcValue(startValue, finalValue, interval, duration) {
-    return startValue + interval * (finalValue - startValue)/duration;
-  }
-}
-
-export default TimedAnimation;
